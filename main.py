bfe357ba6fa6149325ba796e7ef4993046ead0a2
MandrikA <78822060+Wonder0Maker@users.noreply.github.com>
2021-04-24 20:08:27 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from top_films import top_films_of_all_times, top_films_of_last_10years, top_films_of_60s
-from connections import write_csv
+import TopFilmsByGenres
 
-if __name__ == '__main__':
-    write_csv(top_films_of_all_times(), 'top_films_of_all_times')
-    write_csv(top_films_of_last_10years(), 'top_films_of_last_10years')
-    write_csv(top_films_of_60s(), 'top_films_of_60s')
+if __name__ == "__main__":
+    TopFilmsByGenres.top_films_by_genres_exec()
+
+
