0699ea347d81645271aa5abc5fd853f6ea4ca8ed
MandrikA <78822060+Wonder0Maker@users.noreply.github.com>
2021-04-24 20:05:06 +0300

    Modified dev

--- venv/connections.py
+++ venv/connections.py
@@ -1,21 +1,18 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName('MyProject').getOrCreate()
+spark = SparkSession.builder.appName('MyProject')\
+    .getOrCreate()
 
-akas = spark.read.csv('D:\\pythonProject1\\venv\\dataset\\title.akas.tsv/data.tsv',
-                      sep=r'\t', header=True)
+titles_clue = spark.read.csv('venv/dataset/title.akas.tsv/data.tsv', sep=r'\t', header=True)
 
-basics = spark.read.csv('D:\\pythonProject1\\venv\\dataset\\title.basics.tsv/data.tsv',
-                        sep=r'\t', header=True)
+titles_info = spark.read.csv('venv/dataset/title.basics.tsv/data.tsv', sep=r'\t', header=True)
 
-crew = spark.read.csv('D:\\pythonProject1\\venv\\dataset\\title.crew.tsv/data.tsv',
-                      sep=r'\t', header=True)
+crew_info = spark.read.csv('venv/dataset/title.crew.tsv/data.tsv', sep=r'\t', header=True)
 
-episode = spark.read.csv('D:\\pythonProject1\\venv\\dataset\\title.episode.tsv/data.tsv',
-                         sep=r'\t', header=True)
+episode_info = spark.read.csv('venv/dataset/title.episode.tsv/data.tsv', sep=r'\t', header=True)
 
-principals = spark.read.csv('D:\\pythonProject1\\venv\\dataset\\title.principals.tsv/data.tsv',
-                            sep=r'\t', header=True)
+cast_info = spark.read.csv('venv/dataset/title.principals.tsv/data.tsv', sep=r'\t', header=True)
 
-ratings = spark.read.csv('D:\\pythonProject1\\venv\\dataset\\title.ratings.tsv/data.tsv',
-                         sep=r'\t', header=True)
+rating_info = spark.read.csv('venv/dataset/title.ratings.tsv/data.tsv', sep=r'\t', header=True)
+
+crew_cast_personal_info = spark.read.csv('venv/dataset/name.basics.tsv/data.tsv', sep=r'\t', header=True)
