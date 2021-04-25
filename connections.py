01aadd6829402c42924a3b2a7c4a9228b5f0daba
Wonder0Maker <78822060+Wonder0Maker@users.noreply.github.com>
2021-04-25 14:48:36 +0300

    Update connections.py

--- connections.py
+++ connections.py
@@ -1,6 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, split, array_contains, explode, dense_rank
-from pyspark.sql.window import Window
 
 spark = SparkSession.builder.appName('MyProject')\
     .getOrCreate()
@@ -18,9 +16,3 @@ cast_info = spark.read.csv('venv/dataset/title.principals.tsv/data.tsv', sep=r'\
 rating_info = spark.read.csv('venv/dataset/title.ratings.tsv/data.tsv', sep=r'\t', header=True)
 
 crew_cast_personal_info = spark.read.csv('venv/dataset/name.basics.tsv/data.tsv', sep=r'\t', header=True)
-
-def write_csv(data_frame, file_name):
-    """ Function for write data frame into csv file"""
-    data_frame.write.format('csv')\
-        .option('header', True).mode('overwrite') \
-        .save('D:\pythonProject1\outputs\outputs' + file_name)
