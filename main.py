cdead9d7abac24d89e422c75c75e4a379a4a5059
MandrikA <78822060+Wonder0Maker@users.noreply.github.com>
2021-04-27 17:12:18 +0300

    modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import top_films
+import connections as con
 
 if __name__ == "__main__":
-    top_films.top_films()
+    con.write_csv(top_films.top_films_of_all_times(), 'top_films_of_all_times')
+    con.write_csv(top_films.top_films_of_last_10years(), 'top_films_of_last_10years')
+    con.write_csv(top_films.top_films_of_60s(), 'top_films_of_60s')
